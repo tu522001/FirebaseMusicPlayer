adfc9edf39e7c8710d130729348a6cfe422446db
XuanTruong <bichkhoai2001@gmail.com>
2023-02-18 18:18:48 +0700

    Commit L8

--- app/src/main/java/com/example/firebasemusicplayer/view/home/ScreenFragment.kt
+++ app/src/main/java/com/example/firebasemusicplayer/view/home/ScreenFragment.kt
@@ -2,10 +2,13 @@ package com.example.firebasemusicplayer.view.home
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.firebasemusicplayer.R
@@ -14,30 +17,17 @@ import com.example.firebasemusicplayer.databinding.FragmentScreenBinding
 import com.example.firebasemusicplayer.model.Music
 import kotlinx.android.synthetic.main.fragment_screen.*
 import java.io.IOException
+import java.text.SimpleDateFormat
 
 
 class ScreenFragment : Fragment() {
 
     private var number: Int = 0
     private var mediaPlayer: MediaPlayer? = null
-    private var musicList: ArrayList<Music>? = null
     private var music: Music? = null
-
-    //
-//    private var database: FirebaseDatabase? = null
-//    private var myRef: DatabaseReference? = null
     private lateinit var binding: FragmentScreenBinding
-//    private var i by Delegates.notNull<Int>()
-
-//    companion object {
-//        private const val ACTION_PLAY = 1
-//        private const val ACTION_SKIP = 2
-//        private const val ACTION_STOP = 3
-//        private const val ACTION_PREVIOUS = 4
-//    }
-
-
     private lateinit var realtimeDatabaseHelper: RealtimeDatabaseHelper
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -51,15 +41,9 @@ class ScreenFragment : Fragment() {
             false
         )
 
-
         realtimeDatabaseHelper = RealtimeDatabaseHelper()
-//
         doSomethingWithListUsers()
-
-
-//        musicList = ArrayList<Music>()
         onClickPosition()
-
 
         return binding.root
     }
@@ -99,17 +83,21 @@ class ScreenFragment : Fragment() {
                 mediaPlayer = MediaPlayer()
                 mediaPlayer!!.setDataSource(musicList!!.get(number!!).songURL)
 
-                // Khai báo biến để lưu trạng thái đang phát nhạc
-                var isPlaying = false
 
+                // Khai báo biến để lưu trạng thái đang phát nhạc
+                var isRepeating = false
 
 //Button Play
                 binding.btnPlay.setOnClickListener {
+
+                    capNhatTimeBaiHat()
                     if (mediaPlayer!!.isPlaying) {
                         // Nếu đang phát -> pause -> đổi hình play
                         mediaPlayer!!.pause()
                         binding.btnPlay.setImageResource(R.drawable.ic_play)
                     } else {
+                        displayTextView()
+
                         /**
                          *   (+) "mediaPlayer!!.currentPosition" : là vị trí hiện tại của mediaPlayer trong quá trình phát nhạc, tính bằng mili giây (ms).
                          *   Nếu giá trị này lớn hơn 0, tức là mediaPlayer đã được phát ít nhất một phần của bài hát.
@@ -119,6 +107,7 @@ class ScreenFragment : Fragment() {
                          *
                          *   (+) "mediaPlayer!!.currentPosition > 0 && mediaPlayer!!.duration != mediaPlayer!!.currentPosition".
                          *   Nếu điều kiện đúng, thì mediaPlayer đang trong trạng thái tạm dừng và chúng ta sẽ gọi mediaPlayer.start() để tiếp tục phát nhạc từ vị trí hiện tại.
+                         *
                          * */
                         if (mediaPlayer!!.currentPosition > 0 && mediaPlayer!!.duration != mediaPlayer!!.currentPosition) {
                             // Đang tạm dừng -> tiếp tục phát
@@ -135,21 +124,25 @@ class ScreenFragment : Fragment() {
                             }
                         }
                         binding.btnPlay.setImageResource(R.drawable.ic_pause)
+                        SetTimeTotal()
+                        capNhatTimeBaiHat()
                     }
                 }
 
 
-// Button Stop
-                binding.btnStop.setOnClickListener {
-                    if (mediaPlayer!!.isPlaying) {
-                        mediaPlayer!!.stop()
-                        mediaPlayer!!.prepare()
+// Button Repeat
+                binding.btnRepeat.setOnClickListener {
+                    binding.btnRepeat.setOnClickListener {
+                        binding.btnRepeat.setImageResource(R.drawable.ic_repeat_one)
+                        isRepeating = !isRepeating
+                        mediaPlayer!!.isLooping = isRepeating
                     }
                 }
 
 
 // Button Next
                 binding.btnNext.setOnClickListener {
+                    displayTextView()
                     if (number!! >= musicList.size - 1) {
                         number = 0
                     } else {
@@ -167,11 +160,14 @@ class ScreenFragment : Fragment() {
                     mediaPlayer!!.start()
                     // Đổi hình ảnh của nút "Play" thành hình "Pause"
                     binding.btnPlay.setImageResource(R.drawable.ic_pause)
+                    SetTimeTotal()
+                    capNhatTimeBaiHat()
                 }
 
 
 // Button Previous
-                binding.btnPrevious.setOnClickListener{
+                binding.btnPrevious.setOnClickListener {
+                    displayTextView()
                     if (number!! <= 0) {
                         number = musicList.size - 1
                     } else {
@@ -189,7 +185,25 @@ class ScreenFragment : Fragment() {
                     mediaPlayer!!.start()
                     // Đổi hình ảnh của nút "Play" thành hình "Pause"
                     binding.btnPlay.setImageResource(R.drawable.ic_pause)
+                    SetTimeTotal()
+                    capNhatTimeBaiHat()
                 }
+
+                // bắt sự kiện trên SeekBak
+                binding.seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+                    override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+
+                    }
+
+                    override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+                    }
+
+                    // chạm vào kéo seekBar xong buông ra thì nó sẽ lấy giá trị seekBar cuối cùng khi buông ra
+                    override fun onStopTrackingTouch(seekBar: SeekBar) {
+                        mediaPlayer!!.seekTo(seekBar.progress)
+                    }
+                })
 
                 Log.d("EEE", "ScreenFragment arrayList : " + musicList!!.size)
             },
@@ -198,6 +212,69 @@ class ScreenFragment : Fragment() {
             }
         )
     }
+
+    private fun displayTextView() {
+        // Hiển thị đi tvStartTime và  tvEndTime khi đang ở màn hình chờ ScreenFragment
+        binding.tvStartTime.visibility = View.VISIBLE
+        binding.tvEndTime.visibility = View.VISIBLE
+    }
+
+    private fun capNhatTimeBaiHat() {
+        realtimeDatabaseHelper.getListUsersFromRealTimeDatabase(
+            onSuccess = { musicList ->
+                val handler = Handler()
+                handler.postDelayed(object : Runnable {
+                    override fun run() {
+                        val dinhDangGio = SimpleDateFormat("mm:ss")
+                        binding.tvStartTime.setText(dinhDangGio.format(mediaPlayer!!.currentPosition))
+
+                        //update progress skSong
+                        binding.seekBar.progress = mediaPlayer!!.currentPosition
+
+
+                        //Kiểm tra thời gian bài hát -> nếu kết thúc  -> next
+                        mediaPlayer!!.setOnCompletionListener {
+                            if (number >= musicList.size - 1) {
+                                number = 0
+                                music = musicList.get(number)
+                            } else {
+                                number++
+                                music = musicList.get(number)
+                            }
+                            // xử lý sự kiện để không bị hát trồng lên nhau
+                            if (mediaPlayer!!.isPlaying) {
+                                mediaPlayer!!.stop()
+                                imageView2.clearAnimation()
+                            }
+                            mediaPlayer!!.setDataSource(musicList!!.get(number!!).songURL)
+                            mediaPlayer!!.start()
+                            binding.btnPlay.setImageResource(R.drawable.ic_pause)
+                            SetTimeTotal()
+                            capNhatTimeBaiHat()
+                        }
+                        handler.postDelayed(this, 500)
+                    }
+                }, 100)
+
+                Log.d("RRR", "musicList : " + musicList.size)
+            },
+            onFailure = { exception ->
+
+            },
+        )
+    }
+
+    private fun SetTimeTotal() {
+        // hàm xử lý sự kiện phút và giây
+        val dinhDangGio = SimpleDateFormat("mm:ss")
+        binding.tvEndTime.setText(dinhDangGio.format(mediaPlayer!!.duration))
+
+        // gán SeeBak = tổng thời gian bài hát
+        // tức là gán max của skSong = mediaPlayer.getDuration()
+        binding.seekBar.max = mediaPlayer!!.duration
+    }
+
+
 
     // button Play
 //    private fun onClickPlayrButton() {
