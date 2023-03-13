b2a91c6cf32c4a1fec84e01dcc4257863bca4a6e
XuanTruong <bichkhoai2001@gmail.com>
2023-03-13 07:37:49 +0700

    commit L17- sign in MVVM

--- app/src/main/java/com/example/firebasemusicplayer/login/SignInFragment.kt
+++ app/src/main/java/com/example/firebasemusicplayer/login/SignInFragment.kt
@@ -15,113 +15,13 @@ import androidx.navigation.fragment.findNavController
 import com.example.firebasemusicplayer.MainActivity
 import com.example.firebasemusicplayer.R
 import com.example.firebasemusicplayer.databinding.FragmentSignInBinding
+import com.example.firebasemusicplayer.model.User
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_sign_in.*
 import kotlinx.android.synthetic.main.fragment_sign_in.view.*
 
 
-
 class SignInFragment : Fragment() {
-
-//    private lateinit var binding: FragmentSignInBinding
-//    private lateinit var mAuth: FirebaseAuth
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//        binding = DataBindingUtil.inflate<FragmentSignInBinding>(
-//            inflater,
-//            R.layout.fragment_sign_in,
-//            container,
-//            false
-//        )
-//        clickSignUp()
-//
-//        mAuth = FirebaseAuth.getInstance()
-//        binding.loginbtn.setOnClickListener {
-//            loginUser(emails.text.toString(), passwords.text.toString());
-//        }
-//        return binding.root
-//    }
-//
-//    private fun loginUser(email: String, password: String) {
-//        if (TextUtils.isEmpty(email)) {
-//            emails.setError("Email cannot be empty")
-//            emails.requestFocus()
-//        } else if (TextUtils.isEmpty(password)) {
-//            passwords.setError("Password cannot be empty")
-//            passwords.requestFocus()
-//        } else {
-//            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-//                if (task.isSuccessful) {
-//                    Toast.makeText(
-//                        requireContext(), "User logged in successfully",
-//                        Toast.LENGTH_SHORT
-//                    )
-//                        .show()
-//                    findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
-//                } else {
-//                    Toast.makeText(
-//                        requireContext(), "Log in Error: ",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//            }
-//        }
-//    }
-//
-//    private fun clickSignUp() {
-//        binding.tvRegister.setOnClickListener {
-//            findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
-//        }
-//    }
-//
-//}
-
-//    private lateinit var binding: FragmentSignInBinding
-//    private lateinit var signInViewModel: SignInViewModel
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        binding = DataBindingUtil.inflate<FragmentSignInBinding>(
-//            inflater,
-//            R.layout.fragment_sign_in,
-//            container,
-//            false
-//        )
-//
-//        signInViewModel = ViewModelProvider(this).get(SignInViewModel::class.java)
-//        signInViewModel.init()
-//
-//        binding.loginbtn.setOnClickListener {
-//            signInViewModel.loginUser(emails.text.toString(), passwords.text.toString())
-//                .observe(viewLifecycleOwner, Observer { success ->
-//                    if (success) {
-//                        Toast.makeText(
-//                            requireContext(), "User logged in successfully",
-//                            Toast.LENGTH_SHORT
-//                        )
-//                            .show()
-//                        findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
-//                    } else {
-//                        Toast.makeText(
-//                            requireContext(), "Log in Error: ",
-//                            Toast.LENGTH_SHORT
-//                        ).show()
-//                    }
-//                })
-//        }
-//
-//        binding.tvRegister.setOnClickListener {
-//            findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
-//        }
-//
-//        return binding.root
-//    }
-
 
     private lateinit var binding: FragmentSignInBinding
     private lateinit var viewModel: SignInViewModel
@@ -135,10 +35,11 @@ class SignInFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(SignInViewModel::class.java)
 
         binding.loginbtn.setOnClickListener {
-            viewModel.signIn(binding.emails.text.toString(), binding.passwords.text.toString())
+            val signInData = User(binding.emails.text.toString(), binding.passwords.text.toString())
+            viewModel.authenticateUser(signInData)
         }
 
-        viewModel.signInSuccess.observe(viewLifecycleOwner, Observer { success ->
+        viewModel.isAuthenticated.observe(viewLifecycleOwner, Observer { success ->
             if (success) {
                 Toast.makeText(
                     requireContext(), "User logged in successfully",
--- app/src/main/java/com/example/firebasemusicplayer/login/SignInViewModel.kt
+++ app/src/main/java/com/example/firebasemusicplayer/login/SignInViewModel.kt
@@ -5,46 +5,25 @@ import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.firebasemusicplayer.model.User
 import com.google.firebase.auth.FirebaseAuth
 
 class SignInViewModel : ViewModel() {
-//    private lateinit var mAuth: FirebaseAuth
-//
-//    fun init() {
-//        mAuth = FirebaseAuth.getInstance()
-//    }
-//
-//    fun loginUser(email: String, password: String): LiveData<Boolean> {
-//        val loginResult = MutableLiveData<Boolean>()
-//        if (TextUtils.isEmpty(email)) {
-//            loginResult.value = false
-//        } else if (TextUtils.isEmpty(password)) {
-//            loginResult.value = false
-//        } else {
-//            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-//                if (task.isSuccessful) {
-//                    loginResult.value = true
-//                } else {
-//                    loginResult.value = false
-//                }
-//            }
-//        }
-//        return loginResult
-//    }
 
     private val mAuth = FirebaseAuth.getInstance()
 
-    private val _signInSuccess = MutableLiveData<Boolean>()
-    val signInSuccess: LiveData<Boolean>
-        get() = _signInSuccess
+    private val _isAuthenticated = MutableLiveData<Boolean>()
+    val isAuthenticated: LiveData<Boolean>
+        get() = _isAuthenticated
 
-    fun signIn(email: String, password: String) {
-        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
-            _signInSuccess.value = false
+    fun authenticateUser(user: User) {
+        if (TextUtils.isEmpty(user.email) || TextUtils.isEmpty(user.password)) {
+            _isAuthenticated.value = false
             return
         }
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            _signInSuccess.value = task.isSuccessful
+        mAuth.signInWithEmailAndPassword(user.email,user.password).addOnCompleteListener { task ->
+            _isAuthenticated.value = task.isSuccessful
         }
     }
+
 }
